ac4cd50341ba6e5cd3a1926b1c10d506f79d766d
yuneider parada <yparada22@gmail.com>
2022-06-05 01:15:53 -0500

    Events

--- directivas/src/App.vue
+++ directivas/src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="center">
-    <div :class="input" >
-      <h1 >{{name}}</h1>
-      <input type="text" v-model="input"/>
-      <h3 v-text="code"></h3>
-      <img :src="img"/>
-    </div>  
+  <div>
+    <div> 
+      <div :style="{left:left+'px',top:top+'px'}" :class="mode" id="div">
+        <h1 >{{name}}</h1>
+        <select @change="changeMode()" v-model="selectedMode">
+          <option value="darkMode">Dark Mode</option>
+          <option value="lightMode">Light Mode</option>
+        </select>
+        <h3 v-text="code"></h3>
+        <img :src="img"/>
+      </div> 
+    </div>
+    <div class="button">
+      <button @click="moveToLeft()" >LEFT</button>
+      <button @click="moveToRight()" >RIGHT</button>
+      <button @click="moveToUp()" >UP</button>
+      <button @click="moveToDown()" >DOWN</button>
+    </div>
   </div>
 </template>
 
@@ -16,35 +27,61 @@ export default{
     return{
       name: 'Yuneider Parada Pérez',
       code: '20191020058',
-      input: 'darkMode o lightMode',
-      img: "https://picsum.photos/200/300",
-      grid: "grid"
+      img: 'https://picsum.photos/200/300',
+      grid: 'grid',
+      selectedMode: 'Light Mode',
+      mode: 'lightMode',
+      left: 0,
+      top: 0,
     }
+  },
+  methods:{
+    changeMode(){
+      this.mode = this.selectedMode;
+    },
+    moveToLeft(){
+      this.left -= 10;
+    },
+    moveToRight(){
+      this.left += 10;
+    },  
+    moveToUp(){
+      this.top -= 10;
+    },
+    moveToDown(){
+      this.top += 10;
+    },
   },
 }
 </script>
 
 
 <style>
-.center{
-  display: flex;
+.button{
+  position: absolute;
+  z-index: 1;
 
-  justify-content: center;
-  align-items: center;
+  left: 45%;
 }
 .darkMode{
   display: grid;
+  position: absolute;
+
+  width: 400px;
 
   margin-top: 50px;
   padding: 50px;
 
-  place-items: center;
+  place-items: center; 
 
   color: lightgray;
   background: darkslategray;
 }
 .lightMode{
   display: grid;
+  position: absolute;
+
+  width: 400px;
 
   margin-top: 50px;
   padding: 50px;
